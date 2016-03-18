919a0e8b10396a413afc90c29f35c82ecabe6740
David Walsh <davidwalsh83@gmail.com>
2016-03-18 06:33:17 -0500

    Progress update

--- wp-offline-content/class-wp-offline-content-admin.php
+++ wp-offline-content/class-wp-offline-content-admin.php
@@ -17,6 +17,8 @@ class WP_Offline_Content_Admin {
         return self::$instance;
     }
 
+    public $smo;
+
     private $options;
 
     private function __construct() {
@@ -78,10 +80,24 @@ class WP_Offline_Content_Admin {
     }
 
     public function admin_menu() {
-        add_options_page(
-            __('Offline Content Options', 'offline-content'), __('Offline Content', 'offline-content'),
-            'manage_options', self::$options_page_id, array($this, 'create_content_admin_page')
-        );
+
+      add_menu_page(
+        'Offline',
+        'Offline',
+        'manage_options',
+        'offline-content',
+        array($this, 'create_content_admin_page')
+      );
+
+      $this->smo = add_submenu_page(
+        'offline-content',
+        'Design',
+        'Design',
+        'manage_options',
+        'offline-content-shell',
+        array($this, 'create_shell_admin_page')
+      );
+
     }
 
     public function create_content_admin_page() {
@@ -89,7 +105,8 @@ class WP_Offline_Content_Admin {
     }
 
     public function create_shell_admin_page() {
-        include_once(plugin_dir_path(__FILE__) . 'wp-offline-shell-admin.php');
+        Offline_Shell_Admin::on_admin_menu($this->smo);
+        Offline_Shell_Admin::options();
     }
 
     public function network_timeout_input() {
