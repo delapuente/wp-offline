b300d19e5590f3b78702d8fe067ee01ec1f6435a
Salvador de la Puente González <salva@unoyunodiez.com>
2016-04-22 14:26:01 +0200

    class-wp-offline-shell-recommender.php passes the linter

--- wp-offline-content/class-wp-offline-shell-recommender.php
+++ wp-offline-content/class-wp-offline-shell-recommender.php
@@ -1,11 +1,29 @@
 <?php
+/**
+ * Includes the class holding the file recommender logic
+ *
+ * @package OfflineContent
+ */
 
+/**
+ * Represents the recommender.
+ *
+ * It suggests the files of a WordPress installation that should be cached to
+ * increase the performance.
+ */
 class Offline_Shell_Recommender {
+	/**
+	 * The singleton instance.
+	 *
+	 * @var Offline_Shell_Recommender
+	 */
 	private static $instance;
 
-	public function __construct() {
-	}
-
+	/**
+	 * Initializes the recommender shared instance.
+	 *
+	 * @returns Offline_Shell_Recommender
+	 */
 	public static function init() {
 		if ( ! self::$instance ) {
 			self::$instance = new self();
@@ -13,6 +31,13 @@ class Offline_Shell_Recommender {
 		return self::$instance;
 	}
 
+	/**
+	 * Decides if there is a minified version of the file.
+	 *
+	 * @param string   $file_name path to the file.
+	 * @param string[] $all_files list of paths.
+	 * @returns bool true if there exist a minified version of the file.
+	 */
 	public static function has_min_file( $file_name, $all_files ) {
 		$exploded_path = explode( '/', $file_name );
 		$immediate_name = array_pop( $exploded_path );
@@ -24,6 +49,13 @@ class Offline_Shell_Recommender {
 		return count( preg_grep( $regex, $all_files ) );
 	}
 
+	/**
+	 * Checks if a file name match one of any regular expressions.
+	 *
+	 * @param string   $file_name the file name to check.
+	 * @param string[] $regexes array of regular expressions.
+	 * @returns bool true if the file name matches any of the regular expressions.
+	 */
 	public static function matches_any_regex( $file_name, $regexes = array() ) {
 		foreach ( $regexes as $regex ) {
 			if ( preg_match( $regex, $file_name ) ) {
